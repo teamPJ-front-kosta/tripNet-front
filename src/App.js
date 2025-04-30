1067355e519d0a3358ed9663efcc3991831ae6fd
hhs5486 <hhs5486@naver.com>
2025-04-30 10:22:20 +0900

    test4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import StayAbroad from './pages/StayAbroad';
-import KoreanGuestHouse from './pages/KoreanGuestHouse';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import StayAbroad from "./pages/StayAbroad";
+import KoreanGuestHouse from "./pages/KoreanGuestHouse";
+import HotelDetail from "./pages/HotelDetail";
+import DomesticAccommodation from "./pages/domesticAccommodation";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          <Route path="/" element={<StayAbroad />} />
-          <Route path="/stay-abroad" element={<StayAbroad />} />
-          <Route path="/korean-guest-house" element={<KoreanGuestHouse />} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<StayAbroad />} />
+            <Route path="/stay-abroad" element={<StayAbroad />} />
+            <Route path="/korean-guest-house" element={<KoreanGuestHouse />} />
+            <Route path="/hotel/:cityCode/:hotelId" element={<HotelDetail />} />
+            <Route path="/domestic-accommodation" element={<DomesticAccommodation/>}/>
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
